4520ebb679b0ea041ace03f5af8ec9943b1e47bd
Noah <62971189+Pyro-coder@users.noreply.github.com>
2023-04-24 16:32:43 -0500

    Fixed problems with unsafe things

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
     implementation("javax.annotation:javax.annotation-api:1.3.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("com.squareup.retrofit2:converter-gson:2.5.0")
-    implementation ("org.apache.xmlgraphics:batik-swing:1.14")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("org.apache.xmlgraphics:batik-all:1.16")
 
 }
 
