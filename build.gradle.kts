1f6aa366a60c710101c30d21a5d6292eb55d1723
Noah <62971189+Pyro-coder@users.noreply.github.com>
2023-04-26 22:19:32 -0500

    Fixed the gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation ("com.google.code.gson:gson:2.10.1")
-    implementation ("org.apache.xmlgraphics:batik-all:1.16")
 
 }
 
